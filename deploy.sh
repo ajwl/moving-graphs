42e49aab8129df1f26363fc821050cf16e2648d1
Anna Leach <anna,leach@theguardian.com>
2019-06-04 12:33:04 +0100

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BUCKET=colour-blocks
+BUILD_DIR=dist
+
+#import variables from .env 
+USER_NAME=$(grep USER_NAME .env | cut -d '=' -f2)
+ACCESS_KEY_ID=$(grep ACCESS_KEY_ID .env | cut -d '=' -f2)
+SECRET_ACCESS_KEY=$(grep SECRET_ACCESS_KEY .env | cut -d '=' -f2)
+REGION=$(grep REGION .env | cut -d '=' -f2)
+
+echo "Connecting to AWS..."
+
+aws configure --profile $USER_NAME
+aws configure set aws_access_key_id $ACCESS_KEY_ID 
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+aws configure set region $REGION
+ 
+echo "Uploading content..."
+
+# #copy all files in build dir into the root directory located in my-s3-bucket 
+aws s3 cp ${BUILD_DIR}/ s3://${BUCKET}/ --recursive
